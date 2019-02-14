b3ff6a64c26c6488a39853978a54bf0da2dc37a7
Elisabeth Miller <libbymiiler09@gmail.com>
2019-02-13 17:51:07 -0800

    fixed deploy problem

--- test/test-app.js
+++ test/test-app.js
@@ -1,72 +1,72 @@
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// // const faker = require('faker');
-// const mongoose = require('mongoose');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+// const faker = require('faker');
+const mongoose = require('mongoose');
 
-// const expect = chai.expect;
-// const should = chai.should();
+const expect = chai.expect;
+const should = chai.should();
 
-// const { closeServer, runServer, app } = require('../app');
-// const { books } = require("../models/Book");
-// const Book = mongoose.model("books");
-// const { TEST_DATABASE_URL } = require('../config/config');
-
-
-// chai.use(chaiHttp);
-
-// function tearDownDb() {
-//   return new Promise((resolve, rejects) => {
-//     console.warn('deleting database');
-//     mongoose.connection.dropDatabase()
-//       .then(result => resolve(result))
-//       .catch(err => rejects(err));
-//   });
-// }
-
-// //function to generate random data for testing
-// function seedBookData() {
-//   console.info('seeding book data');
-//   const seedData = [];
-//   for (let i = 1; i<=10; i++) {
-//     seedData.push({
-//       title: 'Harry Potter and the Chamber of Secrets',
-//       author: 'J.K. Rowling',
-//       genre: 'fantasy',
-//       goalPages: '33',
-//       goalChapters: '3'
-//     });
-//   }
-//   return Book.insertMany(seedData);
-// }
+const { closeServer, runServer, app } = require('../app');
+const { books } = require("../models/Book");
+const Book = mongoose.model("books");
+const { TEST_DATABASE_URL } = require('../config/config');
 
 
-// // test for connection to server
-// describe('Books API resource', () => {
+chai.use(chaiHttp);
 
-//   before(function () {
-//     return runServer(TEST_DATABASE_URL);
-//   });
+function tearDownDb() {
+  return new Promise((resolve, rejects) => {
+    console.warn('deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => rejects(err));
+  });
+}
 
-//   beforeEach(function() {
-//     return seedBookData();
-//   });
+//function to generate random data for testing
+function seedBookData() {
+  console.info('seeding book data');
+  const seedData = [];
+  for (let i = 1; i<=10; i++) {
+    seedData.push({
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
+      genre: 'fantasy',
+      goalPages: '33',
+      goalChapters: '3'
+    });
+  }
+  return Book.insertMany(seedData);
+}
 
-//   afterEach(function() {
-//     return tearDownDb();
-//   });
 
-//   after(function () {
-//     return closeServer();
-//   });
+// test for connection to server
+describe('Books API resource', () => {
 
-//   describe('app', function() {
-//      it('verify that user has hit the root url, index.html', () => {
-//       chai.request(app)
-//         .get('/')
-//         .then(res => {
-//           expect(res).to.be.html;
-//           expect(res).to.have.status(200);
-//         });
-//     }); 
-//   });
-// })
+  before(function () {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function() {
+    return seedBookData();
+  });
+
+  afterEach(function() {
+    return tearDownDb();
+  });
+
+  after(function () {
+    return closeServer();
+  });
+
+  describe('app', function() {
+     it('verify that user has hit the root url, index.html', () => {
+      chai.request(app)
+        .get('/')
+        .then(res => {
+          expect(res).to.be.html;
+          expect(res).to.have.status(200);
+        });
+    }); 
+  });
+})
--- test/test-book.js
+++ test/test-book.js
@@ -1,18 +1,18 @@
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// // const faker = require('faker');
-// const mongoose = require('mongoose');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+// const faker = require('faker');
+const mongoose = require('mongoose');
 
-// const should = chai.should();
-// const expect = chai.expect;
+const should = chai.should();
+const expect = chai.expect;
 
-// const { app, runServer, closeServer } = require('../app');
-// const { books } = require("../models/Book");
-// const Book = mongoose.model("books");
-// const { TEST_DATABASE_URL } = require('../config/config');
+const { app, runServer, closeServer } = require('../app');
+const { books } = require("../models/Book");
+const Book = mongoose.model("books");
+const { TEST_DATABASE_URL } = require('../config/config');
 
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
 // describe('/about endpoint', function() {
 //   it('should verifty user has reached the about page', function() {
@@ -47,96 +47,96 @@
 //   });
 // });
 
-// describe('GET endpoint', function() {
-//   it('Should return all books', function() {
-//     let res;
-//     return chai.request(app).get('/')
-//       .then(_res => {
-//         res = _res;
-//         expect(res).to.have.status(200);
-//       })
-//   });
+describe('GET endpoint', function() {
+  it('Should return all books', function() {
+    let res;
+    return chai.request(app).get('/')
+      .then(_res => {
+        res = _res;
+        expect(res).to.have.status(200);
+      })
+  });
 
-//   it('should GET book by id', function() {
-//     let resBook;
-//     return chai.request(app).get('/')
-//       .then(res => {
-//         expect(res).to.have.status(200);
-//         // expect(res).to.be.json;
-//       })
-//       resBook = res.body;
-//       return Book.findById(resBook.id);
-//   })
-//   // .then(book => {
-//   //   expect(resBook.id).to.equal(book.id);
-//   // });
-// });
+  it('should GET book by id', function() {
+    let resBook;
+    return chai.request(app).get('/')
+      .then(res => {
+        expect(res).to.have.status(200);
+        // expect(res).to.be.json;
+      })
+      resBook = res.body;
+      return Book.findById(resBook.id);
+  })
+  // .then(book => {
+  //   expect(resBook.id).to.equal(book.id);
+  // });
+});
 
-// describe('POST endpoint', function() {
-//   it('should add a new book', function() {
-//     let book = {
-//       title : 'new title',
-//       author : 'new author',
-//       genre : 'new genre',
-//       goalPages : 'new goalPages',
-//       goalChapters : 'new goalChapters'
-//     }
-//     return chai.request(app)
-//         .post('/books')
-//         .send(book)
-//         .then(function(res) {
-//           expect(res).to.have.status(200);
-//           expect(res.body).to.be.a('object');
-//         })
-//     })
-// });
+describe('POST endpoint', function() {
+  it('should add a new book', function() {
+    let book = {
+      title : 'new title',
+      author : 'new author',
+      genre : 'new genre',
+      goalPages : 'new goalPages',
+      goalChapters : 'new goalChapters'
+    }
+    return chai.request(app)
+        .post('/books')
+        .send(book)
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.a('object');
+        })
+    })
+});
 
-// describe('PUT endpoint', function() {
-//   it('Should update book', function() {
-//     let updatedbook = {
-//       title: 'The Giver',
-//       author: 'Lois Lowry',
-//       genre: 'fiction',
-//       goalPages: '38',
-//       goalChapters: '4'
-//     };
-//     return Book
-//       .findOne()
-//       .then(book => {
-//         updatedbook.id = book.id;
+describe('PUT endpoint', function() {
+  it('Should update book', function() {
+    let updatedbook = {
+      title: 'The Giver',
+      author: 'Lois Lowry',
+      genre: 'fiction',
+      goalPages: '38',
+      goalChapters: '4'
+    };
+    return Book
+      .findOne()
+      .then(book => {
+        updatedbook.id = book.id;
 
-//         return chai.request(app)
-//           .put(`/books/${book.id}`)
-//           .send(updatedbook);
-//       })
-//       .then(function(res) {
-//         expect(res).to.have.status(204);
+        return chai.request(app)
+          .put(`/books/${book.id}`)
+          .send(updatedbook);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
 
-//         return Book.findById(updatedbook.id);
-//       })
-//       .then(function(book) {
-//         expect(book.title).to.equal(updatedbook.title);
-//         expect(book.author).to.equal(updatedbook.author);
-//       });
-//   });
-// });
+        return Book.findById(updatedbook.id);
+      })
+      .then(function(book) {
+        expect(book.title).to.equal(updatedbook.title);
+        expect(book.author).to.equal(updatedbook.author);
+      });
+  });
+});
 
-// describe('DELETE endpoint', function() {
-//   it('should delete a book by its id', function() {
-//       let book;
+describe('DELETE endpoint', function() {
+  it('should delete a book by its id', function() {
+      let book;
 
-//       return Book
-//         .findOne()
-//         .then(_book => {
-//           book = _book;
-//           return chai.request(app).delete(`/books/${book.id}`)
-//         })
-//         .then(res => {
-//           expect(res).to.have.status(204);
-//           return Book.findById(book.id);
-//         })
-//         .then(_book => {
-//           expect(_book).to.be.null;
-//         });
-//   });
-// });
+      return Book
+        .findOne()
+        .then(_book => {
+          book = _book;
+          return chai.request(app).delete(`/books/${book.id}`)
+        })
+        .then(res => {
+          expect(res).to.have.status(204);
+          return Book.findById(book.id);
+        })
+        .then(_book => {
+          expect(_book).to.be.null;
+        });
+  });
+});
